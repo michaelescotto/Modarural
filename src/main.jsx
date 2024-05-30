050c2b1b81a29e2dad53f0693381115f45073027
Michael Escotto <michadacu@gmail.com>
2024-05-30 14:49:52 -0300

    4to commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,12 @@ import App from './App.jsx'
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhTqhi76RR9XIRelIyc8jNe48FHgIgYJU",
-  authDomain: "modarural-escottomichael.firebaseapp.com",
-  projectId: "modarural-escottomichael",
-  storageBucket: "modarural-escottomichael.appspot.com",
-  messagingSenderId: "992683997059",
-  appId: "1:992683997059:web:bb06409b565814e18228b6"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 initializeApp(firebaseConfig);
